778f6e55e853491a8621b90a9c30f588a79278cf
blackwhaledev <alizade.t488@gmail.com>
2022-12-04 17:18:17 +0300

    s1 e1

---
+++ S1-js_refresher/e1.js
@@ -0,0 +1,25 @@
+const x = 'usee'
+let y = "usee"
+
+function oldTypeOfFunction(){}
+
+//arrow function
+const newTypeOfFunction = () => {}
+
+const string = "its string"
+const string1 = 'its string too'
+const string2 = `also its string`
+
+const bool = true // can be true or false
+
+const int = 123 //numbers are integer
+const arr = [1, '1', true] // array/list with keys starts from 0
+const obj = {arr, second_key: 'its the value of the second key'}
+
+//getting items of array:
+arr[0] //will return 1(int)
+arr[1] //will return '1'(string)
+
+//getting items of objects
+obj.arr // will return [1, '1', true]
+obj['second_key'] // will return 'its the value of the second key'
